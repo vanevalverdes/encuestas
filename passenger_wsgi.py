49f4a5daa1cd312e70e3a389826b43408f7cccdf
Vanessa Valverde <vane.valverdesantos@gmail.com>
2025-02-27 00:06:51 -0300

    corrige passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,6 @@
 import sys, os
 
-INTERP = "/home/encuestas25opol/public_html/app/venv/bin/python"
+INTERP = "/home/encuestas25opol/public_html/application/venv/bin/python"
 if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
 
 from index import app as application
