ce349651c0ceb3a2230f34d9de86b2fc004aedf1
Vanessa Valverde <vane.valverdesantos@gmail.com>
2025-04-04 12:02:05 -0300

    Stats with all user to 20

--- utils/methods/stats.py
+++ utils/methods/stats.py
@@ -343,7 +343,7 @@ def generateReport(clazzname,record_id):
                 "Total":tot
         }
         ### User groups
-        userCreation_groups = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"]
+        userCreation_groups = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"]
         userCreation = countbygender(clazzname,"createdby_id",userCreation_groups)
 
         ### party groups
