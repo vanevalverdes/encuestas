f2ca3f5ef480102bcdfa042b0d594ecf6db96d1b
Vanessa C. Valverde <vane.valverdesantos@gmail.com>
2025-06-13 23:22:10 -0400

    Cambia nombre de carpeta en passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,6 @@
 import sys, os
 
-INTERP = "/home/encuestas25opol/public_html/application/venv/bin/python"
+INTERP = "/home/encuestas25opol/public_html/encuestas/venv/bin/python"
 if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
 
 from index import app as application
