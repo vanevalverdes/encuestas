5ed2dbf3909375740d909704034bde89a96d897b
Vanessa C. Valverde <vane.valverdesantos@gmail.com>
2025-10-17 02:33:28 -0300

    Modifica el passenger

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,6 @@
 import sys, os
 
-INTERP = "/home/k4luzun1cocms/public_html/venv/bin/python"
+INTERP = "/home/encuestas25opol/public_html/encuestas/venv/bin/python"
 if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
 
 from index import app as application
